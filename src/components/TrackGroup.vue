196b3d1ba82fb86f3771bdb1dec90bc322ba8437
nosrith <h0.n.xdgz@gmail.com>
2021-06-10 01:05:39 +0900

    feat: change track order by dragging

--- src/components/TrackGroup.vue
+++ src/components/TrackGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <v-group v-if="isLineIntersectingPlotPane">
     <v-line :config="lineConfig"></v-line>
-    <v-text :config="labelConfig" @click="onLabelClick"></v-text>
+    <v-text :config="labelConfig" @click="onLabelClick" @mousedown="onLabelMouseDown"></v-text>
   </v-group>
 </template>
 
@@ -12,6 +12,7 @@ import ViewConfig from "@/data/ViewConfig";
 import ViewState from "@/data/ViewState";
 import Station from "@/data/Station";
 import Track from "@/data/Track";
+import { KonvaEventObject } from "konva/types/Node";
 
 @Component
 export default class TrackGroup extends Vue {
@@ -20,7 +21,9 @@ export default class TrackGroup extends Vue {
   @InjectReactive() diagram!: Diagram;
   @Prop() station!: Station;
   @Prop() track!: Track;
-  
+
+  dragState: { sy0: number, dragging: boolean } | null = null;
+
   get lineConfig(): unknown {
     return {
       points: [
@@ -60,6 +63,44 @@ export default class TrackGroup extends Vue {
     if (this.viewState.editMode && !this.viewState.isInputEnabled) {
       this.viewState.trackNameInputTarget = { stationId: this.station.id, trackId: this.track.id };
       this.$emit("trackNameInputStart");
+    }
+  }
+
+  onLabelMouseDown(konvaEvent: KonvaEventObject<MouseEvent>): void {
+    if (this.viewState.editMode) {
+      this.dragState = { sy0: konvaEvent.evt.screenY, dragging: false };
+      window.addEventListener("mousemove", this.onWindowMouseMove);
+      window.addEventListener("mouseup", this.onWindowMouseUp);
+    }
+  }
+
+  onWindowMouseMove(event: MouseEvent): void {
+    if (this.dragState) {
+      if (Math.abs(event.screenY - this.dragState.sy0) > 1) {
+        this.dragState.dragging = true;
+      }
+      if (this.dragState.dragging) {
+        const currentIndex = this.station.tracks.findIndex(t => t.id == this.track.id);
+        this.station.tracks.splice(currentIndex, 1);
+
+        const mouseRelY = this.diagram.getRelYByY(event.clientY);
+        const newIndex = this.station.tracks.findIndex(t => mouseRelY < t.relY);
+        if (newIndex < 0) {
+          this.station.tracks.push(this.track);
+        } else {
+          this.station.tracks.splice(newIndex, 0, this.track);
+        }
+        this.$emit("updateY");
+      }
+    }
+  }
+
+  onWindowMouseUp(event: MouseEvent): void {
+    if (this.dragState && this.dragState.dragging) {
+      window.removeEventListener("mousemove", this.onWindowMouseMove);
+      window.removeEventListener("mouseup", this.onWindowMouseUp);
+      this.onWindowMouseMove(event);
+      this.dragState = null;
     }
   }
 }
