8aaf3f63a5afc2e849d48910d742707b1bd2eb9e
nosrith <h0.n.xdgz@gmail.com>
2021-06-16 17:39:13 +0900

    refactor: remove unused messages

--- src/messages.ts
+++ src/messages.ts
@@ -1,8 +1,6 @@
 export default {
     ja: {
         message: {
-            confirmRemoveStationRefered: "この駅を使用している列車があります。駅を削除すると、列車の時刻情報も削除されます。よろしいですか？",
-            confirmRemoveTrackRefered: "この番線を使用している列車があります。番線を削除すると、列車の時刻情報も削除されます。よろしいですか？",
             failedToLoadJSON: "ファイルの読み込みに失敗しました。",
             editButtonTooltip: "編集モード",
             undoButtonTooltip: "元に戻す",
